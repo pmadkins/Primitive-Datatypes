59d98abce4b3d2d775f277d7bb6ee963d177fc2c
pmadkins <phil.adkins@icloud.com>
2021-03-01 22:45:48 -0500

    Added casting challenge

--- src/com/adkinslab/Main.java
+++ src/com/adkinslab/Main.java
@@ -52,5 +52,23 @@ public class Main {
         byte myByte = (byte) (myMinByteValue / 2);
 
 
+        /*Challenge
+        1) Create a byte variable and set it to any valid byte number.
+        2) Create a short variable and set it to any valid short number.
+        3) Create an int variable and set it to any valid int number.
+        4) Create a long variable and make it equal to 50000 plus
+           10 times the sum of the byte plus the short plus the int values.
+         */
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+
+
+
+
+
     }
 }
